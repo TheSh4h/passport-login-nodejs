1e91fc119eb2232c032f20cc85ad981c0457c6d3
TheSh4h <hh.haseebshah@gmail.com>
2024-07-09 16:47:51 +0500

    forms layed out

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', { name: 'slayer' })
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+
+})
+
+app.listen(3000)
